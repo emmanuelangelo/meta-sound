fb990fe958a6ead8b0fe39fa9a89b233fa8c6a43
Max Maduka <chinonsomaduka2000@gmail.com>
2022-03-13 19:47:38 +0100

    first commit

---
+++ src/components/footer-components/CTAbanner.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PromptButton from '../featured-components/PromptButton'
+export default function CTAbanner() {
+    return (
+        <div className='CTA-banner'>
+            <div className="cta-content">
+                <h2 style={{
+                    fontSize: '12px',
+                    fontWeight: '400',
+                    lineHeight: '16px',
+                    letterSpacing: '.1em',
+                    textTransform: 'uppercase',
+                    marginBottom: '4px'
+                }}>Meta Sound</h2>
+                <h3 style={{
+                    fontSize: '16px',
+                    fontWeight: '500',
+                    lineHeight: '24px',
+                    letterSpacing: 'normal',
+                    textTransform: 'none'
+                }}>Pure Demo, for Meta Sound, Music in the meta verse.</h3>
+            </div>
+            <PromptButton to='https://spotify.com/signup' name='Sign up free' styleName='CTA'/>
+        </div>
+    )
+}
